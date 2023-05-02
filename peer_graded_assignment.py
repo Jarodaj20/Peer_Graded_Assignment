94ed4b9df33d0fddff9d1ac437215fe40c5e19d5
Jarod Johnson <86135919+Jarodaj20@users.noreply.github.com>
2023-05-02 11:02:14 -0500

    Update peer_graded_assignment.py

--- peer_graded_assignment.py
+++ peer_graded_assignment.py
@@ -142,11 +142,11 @@ app.layout = html.Div(children=[
 # Callback function definition
 # TASK4: Add 5 ouput components
 # Enter your code below. Make sure you have correct formatting.
-@app.callback([[Output(component_id='plot1', component_property='children'),
+@app.callback([Output(component_id='plot1', component_property='children'),
                 Output(component_id='plot2', component_property='children'),
                 Output(component_id='plot3', component_property='children'),
                 Output(component_id='plot4', component_property='children'),
-                Output(component_id='plot5', component_property='children')]
+                Output(component_id='plot5', component_property='children')
                ],
               [Input(component_id='input-type', component_property='value'),
                Input(component_id='input-year', component_property='value')],
